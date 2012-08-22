7ef73cbee49ba0fc67db7dd80288ef78afe7e9d6
Jstack <jstack@gisplanning.com>
2012-08-21 17:05:12 -0700

    styled dashboard and advertisng

--- SizeUp.Web/Areas/Widget/Controllers/DashboardController.cs
+++ SizeUp.Web/Areas/Widget/Controllers/DashboardController.cs
@@ -5,7 +5,10 @@ using System.Web;
 using System.Web.Mvc;
 using SizeUp.Web.Controllers;
 using SizeUp.Web.Models;
-
+using SizeUp.Data;
+using SizeUp.Core.Web;
+using SizeUp.Web.Areas.Api.Models;
+using Microsoft.SqlServer.Types;
 namespace SizeUp.Web.Areas.Widget.Controllers
 {
     public class DashboardController : BaseController
@@ -15,9 +18,54 @@ namespace SizeUp.Web.Areas.Widget.Controllers
 
         public ActionResult Index()
         {
-            ViewBag.Header.ActiveTab = NavItems.Dashboard;
-            return View();
-        }
+            using (var context = ContextFactory.SizeUpContext)
+            {
+                ViewBag.Strings = context.ResourceStrings.Where(i => i.Name.StartsWith("Dashboard")).ToDictionary(i => i.Name, i => i.Value);
+                var ind = context.Industries.Where(i => i.Id == CurrentInfo.CurrentIndustry.Id)
+                    .Select(i => new
+                    {
+                        Industry = i,
+                        NAICS6 = context.SicToNAICSMappings.Where(m => m.IndustryId == i.Id).Select(m => m.NAICS).FirstOrDefault(),
+                        NAICS4 = context.NAICS.Where(n => n.NAICSCode == context.SicToNAICSMappings
+                            .Where(m => m.IndustryId == i.Id)
+                            .Select(m => m.NAICS)
+                            .FirstOrDefault()
+                            .NAICSCode.Substring(0, 4)
+                       ).FirstOrDefault()
+                    })
+                    .FirstOrDefault();
 
+
+                ViewBag.Report = new Models.Business.Report()
+                {
+                    CurrentPlace = CurrentInfo.CurrentPlace,
+                    IndustryDetails = new Models.Business.IndustryDetails()
+                    {
+                        Industry = new Areas.Api.Models.Industry.Industry()
+                        {
+                            Id = ind.Industry.Id,
+                            Name = ind.Industry.Name,
+                            SEOKey = ind.Industry.SEOKey
+                        },
+                        NAICS6 = new Areas.Api.Models.NAICS.NAICS()
+                        {
+                            Id = ind.NAICS6.Id,
+                            NAICSCode = ind.NAICS6.NAICSCode,
+                            Name = ind.NAICS6.Name
+                        },
+                        NAICS4 = new Areas.Api.Models.NAICS.NAICS()
+                        {
+                            Id = ind.NAICS4.Id,
+                            NAICSCode = ind.NAICS4.NAICSCode,
+                            Name = ind.NAICS4.Name
+                        }
+                    }
+                };
+
+                ViewBag.ReportJSON = SizeUp.Core.Serialization.Serializer.ToJSON(ViewBag.Report);
+                ViewBag.Header.ActiveTab = NavItems.Dashboard;
+                return View();
+            }
+        }
     }
 }
