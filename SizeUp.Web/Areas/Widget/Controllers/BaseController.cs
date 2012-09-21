190fb5c48c85119ece098daa8f917812ef99ff16
Jstack <jstack@gisplanning.com>
2012-09-20 18:55:28 -0700

    added high contrast and some bug fixes

---
+++ SizeUp.Web/Areas/Widget/Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace SizeUp.Web.Areas.Widget.Controllers
+{
+    public class BaseController : SizeUp.Web.Controllers.BaseController
+    {
+        //
+        // GET: /Widget/Base/
+
+        protected override void Initialize(System.Web.Routing.RequestContext requestContext)
+        {
+            var c = requestContext.HttpContext.Request.Cookies["theme"];
+            var theme = "";
+            if (c != null)
+            {
+                theme = c.Value;
+            }
+            ViewBag.Theme = theme;
+            base.Initialize(requestContext);
+        }
+
+    }
+}
