f74f0b0e82f6694b3fbc68d15ab85f42fb02bc84
Jstack <jstack@gisplanning.com>
2012-12-03 11:25:17 -0800

    writing a promptBox control

---
+++ SizeUp.Web/Scripts/controls/promptBox.js
@@ -0,0 +1,46 @@
+﻿(function () {
+    sizeup.core.namespace('sizeup.controls');
+    sizeup.controls.promptBox = function (opts) {
+
+        var defaults = {
+            textbox: $('<input type="text" />')
+        };
+        var me = {};
+        me.opts = $.extend(true, defaults, opts);
+        me.data = {};
+
+        var init = function () {
+            me.data.promptText = me.opts.textbox.val();
+            me.opts.textbox.blur(onBlur);
+            me.opts.textbox.focus(onFocus);
+        };
+
+        var onBlur = function () {
+
+        };
+
+        var onFocus = function () {
+
+        };
+
+        var getValue = function () {
+            return me.opts.textbox.val();
+        };
+
+        var setValue = function (val) {
+            me.opts.textbox.val(val);
+        };
+
+        var publicObj = {
+            getValue: function () {
+                return getValue();
+            },
+            setValue: function (val) {
+                setValue(val);
+            }
+        };
+        init();
+        return publicObj;
+
+    };
+})();
