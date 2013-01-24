abed45f550d71fc5a0483c8ed51efa5794ec99aa
Jstack <jstack@gisplanning.com>
2013-01-23 16:27:41 -0800

    added core files for mailchimp api

---
+++ SizeUp.Core/Email/IMailingList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SizeUp.Core.Email
+{
+    public interface IMailingList
+    {
+        public bool Subscribe(string email);
+        public bool Unsubscribe(string email);
+        public bool Subscribe(List<string> emails);
+        public bool Unsubscribe(List<string> emails);
+    }
+}
---
+++ SizeUp.Core/Email/MailChimp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailChimp;
+
+namespace SizeUp.Core.Email
+{
+    public class MailChimpMailingList : IMailingList
+    {
+        protected MCApi API { get; set; }
+        public MailChimpMailingList()
+        {
+            API = new MCApi("test", true);
+        }
+        
+        public bool Subscribe(string email)
+        {
+           //return API.ListSubscribe(
+        }
+
+        public bool Unsubscribe(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Subscribe(List<string> emails)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Unsubscribe(List<string> emails)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
