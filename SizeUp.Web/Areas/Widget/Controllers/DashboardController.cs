d171604c9d33dec2c05a12b5c21d66733ef710c5
Jstack <jstack@gisplanning.com>
2013-03-08 15:44:12 -0800

    more clean up of old crap

--- SizeUp.Web/Areas/Widget/Controllers/DashboardController.cs
+++ SizeUp.Web/Areas/Widget/Controllers/DashboardController.cs
@@ -21,48 +21,6 @@ namespace SizeUp.Web.Areas.Widget.Controllers
             using (var context = ContextFactory.SizeUpContext)
             {
                 ViewBag.Strings = context.ResourceStrings.Where(i => i.Name.StartsWith("Dashboard")).ToDictionary(i => i.Name, i => i.Value);
-                var ind = context.Industries.Where(i => i.Id == CurrentInfo.CurrentIndustry.Id)
-                    .Select(i => new
-                    {
-                        Industry = i,
-                        NAICS6 = context.SicToNAICSMappings.Where(m => m.IndustryId == i.Id).Select(m => m.NAICS).FirstOrDefault(),
-                        NAICS4 = context.NAICS.Where(n => n.NAICSCode == context.SicToNAICSMappings
-                            .Where(m => m.IndustryId == i.Id)
-                            .Select(m => m.NAICS)
-                            .FirstOrDefault()
-                            .NAICSCode.Substring(0, 4)
-                       ).FirstOrDefault()
-                    })
-                    .FirstOrDefault();
-
-
-                ViewBag.Report = new Models.Business.Report()
-                {
-                    //CurrentPlace = CurrentInfo.CurrentPlace,
-                    IndustryDetails = new Models.Business.IndustryDetails()
-                    {
-                        Industry = new Areas.Api.Models.Industry.Industry()
-                        {
-                            Id = ind.Industry.Id,
-                            Name = ind.Industry.Name,
-                            SEOKey = ind.Industry.SEOKey
-                        },
-                        NAICS6 = new Areas.Api.Models.NAICS.NAICS()
-                        {
-                            Id = ind.NAICS6.Id,
-                            NAICSCode = ind.NAICS6.NAICSCode,
-                            Name = ind.NAICS6.Name
-                        },
-                        NAICS4 = new Areas.Api.Models.NAICS.NAICS()
-                        {
-                            Id = ind.NAICS4.Id,
-                            NAICSCode = ind.NAICS4.NAICSCode,
-                            Name = ind.NAICS4.Name
-                        }
-                    }
-                };
-
-                ViewBag.ReportJSON = SizeUp.Core.Serialization.Serializer.ToJSON(ViewBag.Report);
                 ViewBag.Header.ActiveTab = NavItems.Dashboard;
                 return View();
             }
