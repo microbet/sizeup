04b7eb2be41682452caa4b5e6dd9dad894a46423
Jstack <jstack@gisplanning.com>
2013-03-21 18:05:48 -0700

    couple bug fixes and rewired conneections trings

--- SizeUp.Core/DataLayer/TotalEmployees.cs
+++ SizeUp.Core/DataLayer/TotalEmployees.cs
@@ -16,28 +16,24 @@ namespace SizeUp.Core.DataLayer
         {
             BarChartItem<long?> output = null;
 
-            var cityData = IndustryData.City(context)
+            var cityData = IndustryData.CityMinBusinessCount(context)
                 .Where(i => i.IndustryId == industryId)
                 .Where(i => i.City.CityCountyMappings.Any(m => m.Id == placeId))
-                .Where(i => i.City.BusinessDataByCities.Where(b => b.IndustryId == i.IndustryId && b.Business.IsActive).Count() >= MinimumBusinessCount)
                 .Where(i => i.TotalEmployees != null && i.TotalEmployees > 0);
 
-            var countyData = IndustryData.County(context)
+            var countyData = IndustryData.CountyMinBusinessCount(context)
                 .Where(i => i.IndustryId == industryId)
                 .Where(i => i.County.CityCountyMappings.Any(m => m.Id == placeId))
-                .Where(i => i.County.BusinessDataByCounties.Where(b => b.IndustryId == i.IndustryId && b.Business.IsActive).Count() >= MinimumBusinessCount)
                 .Where(i => i.TotalEmployees != null && i.TotalEmployees > 0);
 
-            var metroData = IndustryData.Metro(context)
+            var metroData = IndustryData.MetroMinBusinessCount(context)
                 .Where(i => i.IndustryId == industryId)
                 .Where(i => i.Metro.Counties.Any(m => m.CityCountyMappings.Any(mp => mp.Id == placeId)))
-                .Where(i => i.Metro.BusinessDataByCounties.Where(b => b.IndustryId == i.IndustryId && b.Business.IsActive).Count() >= MinimumBusinessCount)
                 .Where(i => i.TotalEmployees != null && i.TotalEmployees > 0);
 
-            var stateData = IndustryData.State(context)
+            var stateData = IndustryData.StateMinBusinessCount(context)
                 .Where(i => i.IndustryId == industryId)
                 .Where(i => i.State.Counties.Any(m => m.CityCountyMappings.Any(mp => mp.Id == placeId)))
-                .Where(i => i.State.BusinessDataByCounties.Where(b => b.IndustryId == i.IndustryId && b.Business.IsActive).Count() >= MinimumBusinessCount)
                 .Where(i => i.TotalEmployees != null && i.TotalEmployees > 0);
 
             var nationData = IndustryData.Nation(context)
@@ -114,7 +110,7 @@ namespace SizeUp.Core.DataLayer
             if (granularity == Granularity.ZipCode)
             {
                 var entities = Base.ZipCode.In(context, placeId, boundingGranularity);
-                var data = IndustryData.ZipCode(context).Where(i => i.IndustryId == industryId);
+                var data = IndustryData.ZipCodeMinBusinessCount(context).Where(i => i.IndustryId == industryId);
                 values =
                     data.Join(entities, i => i.ZipCodeId, i => i.Id, (d, e) => d)
                     .Select(i => i.TotalEmployees);
@@ -122,7 +118,7 @@ namespace SizeUp.Core.DataLayer
             else if (granularity == Granularity.County)
             {
                 var entities = Base.County.In(context, placeId, boundingGranularity);
-                var data = IndustryData.County(context).Where(i => i.IndustryId == industryId);
+                var data = IndustryData.CountyMinBusinessCount(context).Where(i => i.IndustryId == industryId);
                 values =
                     data.Join(entities, i => i.CountyId, i => i.Id, (d, e) => d)
                     .Select(i => i.TotalEmployees);
@@ -130,7 +126,7 @@ namespace SizeUp.Core.DataLayer
             else if (granularity == Granularity.State)
             {
                 var entities = Base.State.In(context, placeId, boundingGranularity);
-                var data = IndustryData.State(context).Where(i => i.IndustryId == industryId);
+                var data = IndustryData.StateMinBusinessCount(context).Where(i => i.IndustryId == industryId);
                 values =
                     data.Join(entities, i => i.StateId, i => i.Id, (d, e) => d)
                     .Select(i => i.TotalEmployees);
--- SizeUp.Web/Areas/Tiles/Controllers/TotalEmployeesController.cs
+++ SizeUp.Web/Areas/Tiles/Controllers/TotalEmployeesController.cs
@@ -40,7 +40,7 @@ namespace SizeUp.Web.Areas.Tiles.Controllers
                 if (granularity == Granularity.ZipCode)
                 {
                     var entities = Core.DataLayer.Base.ZipCode.In(context, placeId, boundingGranularity);
-                    var data = IndustryData.ZipCode(context).Where(i => i.IndustryId == industryId);
+                    var data = IndustryData.ZipCodeMinBusinessCount(context).Where(i => i.IndustryId == industryId);
                     values = entities.GroupJoin(data, i => i.Id, i => i.ZipCodeId, (e, d) => new KeyValue<DbGeography, long?>
                     {
                         Key = e.ZipCodeGeographies.Where(g => g.GeographyClass.Name == Core.Geo.GeographyClass.Display)
@@ -51,7 +51,7 @@ namespace SizeUp.Web.Areas.Tiles.Controllers
                 else if (granularity == Granularity.County)
                 {
                     var entities = Core.DataLayer.Base.County.In(context, placeId, boundingGranularity);
-                    var data = IndustryData.County(context).Where(i => i.IndustryId == industryId);
+                    var data = IndustryData.CountyMinBusinessCount(context).Where(i => i.IndustryId == industryId);
                     values = entities.GroupJoin(data, i => i.Id, i => i.CountyId, (e, d) => new KeyValue<DbGeography, long?>
                     {
                         Key = e.CountyGeographies.Where(g => g.GeographyClass.Name == Core.Geo.GeographyClass.Display)
@@ -62,7 +62,7 @@ namespace SizeUp.Web.Areas.Tiles.Controllers
                 else if (granularity == Granularity.State)
                 {
                     var entities = Core.DataLayer.Base.State.In(context, placeId, boundingGranularity);
-                    var data = IndustryData.State(context).Where(i => i.IndustryId == industryId);
+                    var data = IndustryData.StateMinBusinessCount(context).Where(i => i.IndustryId == industryId);
                     values = entities.GroupJoin(data, i => i.Id, i => i.StateId, (e, d) => new KeyValue<DbGeography, long?>
                     {
                         Key = e.StateGeographies.Where(g => g.GeographyClass.Name == Core.Geo.GeographyClass.Display)
