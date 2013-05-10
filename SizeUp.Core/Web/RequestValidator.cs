cb8828f1e5e59d17ae580eeea7a7438633bd1252
Jstack <jstack@gisplanning.com>
2013-05-10 14:36:43 -0700

    added request validator to fix querystring issue when peope type bad data

---
+++ SizeUp.Core/Web/RequestValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Util;
+
+namespace SizeUp.Core.Web
+{
+    public class RequestValidator : System.Web.Util.RequestValidator
+    {
+        protected override bool IsValidRequestString(System.Web.HttpContext context, string value, RequestValidationSource requestValidationSource, string collectionKey, out int validationFailureIndex)
+        {
+            bool outVal = false;
+            validationFailureIndex = -1;  //Set a default value for the out parameter.
+
+            if (requestValidationSource == RequestValidationSource.QueryString)
+            {
+                outVal = true;
+            }
+            else if (requestValidationSource == RequestValidationSource.Form)
+            {
+                outVal = true;
+            }
+            else
+            {
+                outVal = base.IsValidRequestString(context, value, requestValidationSource, collectionKey, out validationFailureIndex);
+            }
+            return outVal;
+        }
+    }
+}
