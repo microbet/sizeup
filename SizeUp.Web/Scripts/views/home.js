d4b0fa7c208f6bc60fdf1c67103b128e496cd398
sroeder85 <sroeder@sizeup.com>
2014-05-17 15:33:31 -0700

    hover button gradient changes, user option menu on upper right corner of new homepage

--- SizeUp.Web/Scripts/views/home.js
+++ SizeUp.Web/Scripts/views/home.js
@@ -173,29 +173,27 @@
                     next_fs.show();
                     //hide the current fieldset with style
                     current_fs.animate({ opacity: 0 }, {
-                            step: function (now, mx) {
-                                //as the opacity of current_fs reduces to 0 - stored in "now"   
-                                //1. scale current_fs down to 80%
-                                scale = 1 - (1 - now) * 0.2;
-                                //2. bring next_fs from the right(50%)
-                                left = (now * 50) + "%";
-                                //3. increase opacity of next_fs to 1 as it moves in
-                                opacity = 1 - now;
-                                current_fs.css({ 'transform': 'scale(' + scale + ')' });
-                                next_fs.css({ 'left': left, 'opacity': opacity, 'top': '22.5em', 'width': '0%' });
-                        
-                            },
-                            duration: 1,
-                            complete: function () {
-                                current_fs.hide();
-                                animating = false;
-                                next_fs.css({ 'position': 'inherit', 'margin-top': '2.6em', 'width': '100%' });
-                                $("#step-two .container").toggleClass("flipped");
-                            },
-                            //this comes from the custom easing plugin
-                        
-                            easing: 'linear'
-                        });
+                        step: function (now, mx) {
+                            //as the opacity of current_fs reduces to 0 - stored in "now"   
+                            //1. scale current_fs down to 80%
+                            scale = 1 - (1 - now) * 0.2;
+                            //2. bring next_fs from the right(50%)
+                            left = (now * 50) + "%";
+                            //3. increase opacity of next_fs to 1 as it moves in
+                            opacity = 1 - now;
+                            current_fs.css({ 'transform': 'scale(' + scale + ')' });
+                            next_fs.css({ 'left': left, 'opacity': opacity, 'top': '22.5em', 'width': '0%' });
+                        },
+                        duration: 1,
+                        complete: function () {
+                            current_fs.hide();
+                            animating = false;
+                            next_fs.css({ 'position': 'inherit', 'margin-top': '2.6em', 'width': '100%' });
+                            $("#step-two .container").toggleClass("flipped");
+                        },
+                        //this comes from the custom easing plugin
+                        easing: 'linear'
+                    });
                 }
             }
         };
