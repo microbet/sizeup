ca9a224faf696ec8c2a93de5b8753d5dc4ad4609
sroeder85 <sroeder@sizeup.com>
2014-06-26 15:08:35 -0700

    Adding back responsive section and initial page for demo (italy/germany deutsche bank)

---
+++ SizeUp.Web/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SizeUp.Data;
+using SizeUp.Core.Web;
+using SizeUp.Core.Geo;
+using SizeUp.Core.Serialization;
+using Api = SizeUp.Web.Areas.Api;
+
+namespace SizeUp.Web.Controllers
+{
+    public class DemoController : BaseController
+    {
+        public string authToken = "ftyTAnI86i";
+        List<string> countries = new List<string>() { "italy", "germany"};
+        //
+        // GET: /Demo/
+
+        public ActionResult Index()
+        {
+            var country = Request.QueryString["c"];
+            var token = Request.QueryString["t"];
+
+            if(string.IsNullOrEmpty(token) || string.IsNullOrEmpty(country) || token != authToken || !countries.Contains(country) )
+                return Redirect("/");
+
+            return View();
+        }
+
+    }
+}
