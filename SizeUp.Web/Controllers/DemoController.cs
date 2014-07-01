3bf87ee63338542c0b06aff50d8ba4620d0b104d
sroeder85 <sroeder@sizeup.com>
2014-06-30 23:30:38 -0700

    demo for italy/germany

--- SizeUp.Web/Controllers/DemoController.cs
+++ SizeUp.Web/Controllers/DemoController.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SizeUp.Web.Models;
 using SizeUp.Data;
 using SizeUp.Core.Web;
-using SizeUp.Core.Geo;
-using SizeUp.Core.Serialization;
-using Api = SizeUp.Web.Areas.Api;
+using Microsoft.SqlServer.Types;
 
 namespace SizeUp.Web.Controllers
 {
@@ -26,8 +25,61 @@ namespace SizeUp.Web.Controllers
             if(string.IsNullOrEmpty(token) || string.IsNullOrEmpty(country) || token != authToken || !countries.Contains(country) )
                 return Redirect("/");
 
+            ViewBag.Country = country;
+
             return View();
         }
+
+
+        public ActionResult Test()
+        {
+            //using (var context = ContextFactory.SizeUpContext)
+            //{
+            //    ViewBag.Strings = context.ResourceStrings.Where(i => i.Name.StartsWith("Dashboard")).ToDictionary(i => i.Name, i => i.Value);
+            //    ViewBag.Header.ActiveTab = NavItems.Dashboard;
+            //    return View();
+            //}
+            //ViewBag.Country = "italy";
+            //return View();
+
+            var country = Request.QueryString["c"];
+            var token = Request.QueryString["t"];
+
+            if (string.IsNullOrEmpty(token) || string.IsNullOrEmpty(country) || token != authToken || !countries.Contains(country))
+                return Redirect("/");
+
+            ViewBag.Country = country;
+            if (country == "italy")
+            {
+                ViewBag.Industry = "Shoe & Footwear Stoes";
+                ViewBag.Location = "Varese";
+                ViewBag.RowATitle = "Citta";
+                ViewBag.RowBTitle = "Provincia";
+                ViewBag.RowCTitle = "Regione";
+                ViewBag.RowDTitle = "Nazione";
+                ViewBag.RowA = "Varese";
+                ViewBag.RowB = "Varese";
+                ViewBag.RowC = "Lombardia";
+                ViewBag.RowD = "Italia";
+            }
+
+            if (country == "germany")
+            {
+                ViewBag.Industry = "Food Products & Manufacturers";
+                ViewBag.Location = "Ostalbkreis";
+                ViewBag.RowATitle = "District";
+                ViewBag.RowBTitle = "Region";
+                ViewBag.RowCTitle = "State";
+                ViewBag.RowDTitle = "Nation";
+                ViewBag.RowA = "Ostalbkreis";
+                ViewBag.RowB = "Stuttgart";
+                ViewBag.RowC = "Baden-W&uuml;rttemberg";
+                ViewBag.RowD = "Deutschland";
+            }
+
+            return View();
+        }
+
 
     }
 }
