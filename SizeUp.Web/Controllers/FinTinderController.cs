c9d5edcd49bb5c341de06c70f0b96c09292000d0
acooper <acooper@sizeup.com>
2016-04-01 17:02:11 -0700

    fin tiiiiindaaa

---
+++ SizeUp.Web/Controllers/FinTinderController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SizeUp.Web.Controllers
+{
+    public class FinTinderController : Controller
+    {
+        //
+        // GET: /FinTinder/
+
+        public ActionResult Index()
+        {
+            ViewBag.Header = new Models.Header()
+            {
+                HideNavigation = true
+            };
+            return View();
+        }
+
+    }
+}
