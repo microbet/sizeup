db0b4ce604f8ae8e0b46acb678797b5f126f8ba9
acooper <acooper@sizeup.com>
2016-07-28 11:49:41 -0700

    Added GeographyBoundary to tile controller for mobile

--- SizeUp.Api/Areas/Tiles/Controllers/TileController.cs
+++ SizeUp.Api/Areas/Tiles/Controllers/TileController.cs
@@ -109,6 +109,37 @@ namespace SizeUp.Api.Areas.Tiles.Controllers
                 return File(stream.GetBuffer(), "image/png");
             }
         }
+
+        public ActionResult GeographyBoundary(int x, int y, int zoom, long geographicLocationId, int width = 256, int height = 256)
+        {
+            using (var context = ContextFactory.SizeUpContext)
+            {
+                GeographyBoundary tile = new GeographyBoundary(width, height, x, y, zoom);
+                BoundingBox boundingBox = tile.GetBoundingBox(TileBuffer);
+                double tolerance = GetPolygonTolerance(zoom);
+                var boundingGeo = boundingBox.GetDbGeography();
+
+                var geos = context.GeographicLocations
+                    .Where(i => i.Id == geographicLocationId)
+                    .SelectMany(i => i.Geographies.Where(g => g.GeographyClass.Name == Core.Geo.GeographyClass.Display)
+                    .Select(g => SqlSpatialFunctions.Reduce(g.Polygon, tolerance).Intersection(boundingGeo)))
+                    .Where(g => g != null)
+                    .ToList()
+                    .Select(g => new GeographyEntity()
+                    {
+                        Geography = SqlGeography.Parse(g.AsText()),
+                        BorderColor = "#6495ED ",
+                        BorderOpacity = 200,
+                        BorderWidth = 2
+                    })
+                    .ToList();
+
+                tile.Draw(geos);
+                var stream = new System.IO.MemoryStream();
+                tile.Bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return File(stream.GetBuffer(), "image/png");
+            }
+        }
         public ActionResult Businesses(int x, int y, int zoom, List<long> industryIds = null, string color = "#ff5522 ", int width = 256, int height = 256)
         {
             using (var context = ContextFactory.SizeUpContext)
