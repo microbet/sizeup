a2eefdc1d782d0c528e36a71e7233228c40daeb7
acooper <acooper@sizeup.com>
2016-08-23 11:04:47 -0700

    added code to look up bounding city

--- SizeUp.Core/DataLayer/Place.cs
+++ SizeUp.Core/DataLayer/Place.cs
@@ -35,7 +35,7 @@ namespace SizeUp.Core.DataLayer
         public static Models.Place GetByBusiness(SizeUpContext context, long businessId)
         {
             return Get(context)
-                .Where(i => i.City.Businesses.Any(bc=>bc.Id == businessId))
+                .Where(i => i.City.Businesses.Any(bc => bc.Id == businessId))
                 .Select(new Projections.Place.Default().Expression)
                 .FirstOrDefault();
         }
@@ -43,12 +43,12 @@ namespace SizeUp.Core.DataLayer
         public static Models.Place GetLegacy(SizeUpContext context, string SEOKey)
         {
             return Get(context)
-                .Where(i => i.City.LegacyCommunitySEOKeys.Any(l=>l.SEOKey == SEOKey))
+                .Where(i => i.City.LegacyCommunitySEOKeys.Any(l => l.SEOKey == SEOKey))
                 .Select(new Projections.Place.Default().Expression)
                 .FirstOrDefault();
         }
 
-        
+
         public static Models.Place Get(SizeUpContext context, string stateSEOKey, string countySEOKey, string citySEOKey, string metroSEOKey = null)
         {
             Models.Place output = null;
@@ -68,10 +68,10 @@ namespace SizeUp.Core.DataLayer
             }
             else if (!string.IsNullOrEmpty(stateSEOKey))
             {
-                 output = Get(context)
-                    .Where(i => i.County.State.SEOKey == stateSEOKey)
-                    .Select(new Projections.Place.State().Expression)
-                    .FirstOrDefault();
+                output = Get(context)
+                   .Where(i => i.County.State.SEOKey == stateSEOKey)
+                   .Select(new Projections.Place.State().Expression)
+                   .FirstOrDefault();
             }
             else if (!string.IsNullOrEmpty(metroSEOKey))
             {
@@ -82,7 +82,7 @@ namespace SizeUp.Core.DataLayer
             }
             return output != null ? output : new Models.Place() { City = new Models.City(), County = new Models.County(), Metro = new Models.Metro(), State = new Models.State() };
         }
-        
+
         public static IQueryable<Models.DistanceEntity<Models.Place>> ListNear(SizeUpContext context, Core.Geo.LatLng latLng)
         {
             var distanceFilter = new DistanceEntity<Data.Place>.DistanceEntityFilter(latLng);
@@ -90,14 +90,38 @@ namespace SizeUp.Core.DataLayer
                 .Select(i => new KeyValue<Data.Place, Geo.LatLng>
                 {
                     Key = i,
-                    Value = i.GeographicLocation.Geographies.AsQueryable().Where(g=>g.GeographyClass.Name == Geo.GeographyClass.Calculation)
+                    Value = i.GeographicLocation.Geographies.AsQueryable().Where(g => g.GeographyClass.Name == Geo.GeographyClass.Calculation)
                     .Select(g => new Geo.LatLng { Lat = g.CenterLat.Value, Lng = g.CenterLong.Value })
                     .FirstOrDefault()
                 })
                 .Select(distanceFilter.Projection)
                 .Select(new Projections.Place.Distance().Expression);
         }
-        
+
+        public static Models.Place GetBoundingCity(SizeUpContext context, Core.Geo.LatLng latLng)
+        {
+            var distanceFilter = new DistanceEntity<Data.Place>.DistanceEntityFilter(latLng);
+            var locations = Core.DataLayer.Geography.Get(context).Where(x =>
+                    x.West <= latLng.Lng && x.East >= latLng.Lng
+                    && x.North >= latLng.Lat && x.South <= latLng.Lat
+                );
+            var count = locations.Count();
+            var ids = locations.Select(x => x.Id);
+            foreach (var location in locations)
+            {
+                var geoId = location.GeographicLocationId;
+                var geographicLocation = Core.DataLayer.GeographicLocation.Get(context).Where(x => x.Id == geoId && x.GranularityId == 4).FirstOrDefault();
+
+                if (geographicLocation != null)
+                {
+                    return Core.DataLayer.Place.Get(context).Where(x => x.GeographicLocation.Id == geoId)
+                        .Select(new Projections.Place.Default().Expression)
+                        .FirstOrDefault();                    
+                }
+            }
+            return null;
+        }
+
         public static List<Models.Place> List(SizeUpContext context, List<long> placeIds)
         {
             return Get(context)
