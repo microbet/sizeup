ff090ff4b5555f80171b89a6fd341e0e4afe2e70
Travis Wilson <twilson@sizeup.com>
2018-03-01 15:43:32 -0800

    Delete a vast number of build artifacts.
    These appear to have been committed accidentally, before we had a liberal
    .gitignore spec.

--- SizeUp.Web/obj/Release/Package/PackageTmp/Scripts/tracking/outboundLinks.js
+++
@@ -1,6 +0,0 @@
-﻿$(document).ready(function () {
-    $('body').delegate('a[target="_blank"]', 'click', function () {
-        var l = $(this);
-        new sizeup.core.analytics().outgoingLink({ category: 'outgoingLinks', label: l.attr('href'), isInteraction: true });
-    });
-});
