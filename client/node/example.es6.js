3c7dbd78c452aa7e51882f55ca27ee10fbc22d15
Tim James <tjames@tjames.com>
2018-06-21 10:50:54 -0700

    Bigger example

--- client/node/example.es6.js
+++ client/node/example.es6.js
@@ -20,5 +20,39 @@ Promise.all([
         geographicLocationId: place.State.Id
     }),
 ]))
+
+const data = sizeup.api.data;
+Promise.all([
+  data.getIndustryBySeokey("shoe-and-boot-repairing"),
+  data.getPlaceBySeokey("california/alameda/oakland-city"),
+  Promise.resolve({revenue: 123123})  // KPIs for user's own business
+]).then(([industry, place, user]) => Promise.all([
+  data.getAverageRevenue({geographicLocationId: place.City.Id, industryId: industry.Id}),
+  data.getAverageRevenue({geographicLocationId: place.County.Id, industryId: industry.Id}),
+  data.getAverageRevenue({geographicLocationId: place.Metro.Id, industryId: industry.Id}),
+  data.getAverageRevenue({geographicLocationId: place.State.Id, industryId: industry.Id}),
+  data.getAverageRevenue({geographicLocationId: place.Nation.Id, industryId: industry.Id}),
+  data.getAverageRevenuePercentile({value: user.revenue, geographicLocationId: place.City.Id, industryId: industry.Id}),
+  data.getAverageRevenuePercentile({value: user.revenue, geographicLocationId: place.County.Id, industryId: industry.Id}),
+  data.getAverageRevenuePercentile({value: user.revenue, geographicLocationId: place.Metro.Id, industryId: industry.Id}),
+  data.getAverageRevenuePercentile({value: user.revenue, geographicLocationId: place.State.Id, industryId: industry.Id}),
+  data.getAverageRevenuePercentile({value: user.revenue, geographicLocationId: place.Nation.Id, industryId: industry.Id}),
+  data.getAverageRevenueBands({granularity:"ZipCode", boundingGeographicLocationId: place.County.Id, bands: 5, industryId: industry.Id}),
+  data.getConsumerExpenditureVariables({parentId:1}),
+  data.getConsumerExpenditureVariables({parentId:32}),
+  data.getConsumerExpenditureVariable({id: 172}),
+  data.getBusinessesByIndustry({
+    industryIds: [10526,9664],
+    geographicLocationId: place.Id, itemCount: 0, page: 1,
+  }),
+  data.getBusinessesByIndustry({
+    industryIds: [10526],
+    geographicLocationId: place.Id, itemCount: 0, page: 1,
+  }),
+  data.getBusinessesByIndustry({
+    industryIds: [9664],
+    geographicLocationId: place.Id, itemCount: 0, page: 1,
+  }),
+]))
 .then(logj)
 .catch(console.error)
